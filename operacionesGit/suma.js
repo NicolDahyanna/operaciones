7fbbfe283f9500f76d69dae930eb2e72c2c1dfd7
david <dnavia18@misena.edu.co>
2023-11-17 19:57:55 -0500

    se agrega operación resta

--- operacionesGit/suma.js
+++ operacionesGit/suma.js
@@ -4,3 +4,10 @@ function sumar() {
   var resultado = parseInt(numero1) + parseInt(numero2);
   document.getElementById("resultado").innerHTML = resultado;
 }
+
+function resta() {
+  var numero1 = document.getElementById("num3").value;
+  var numero2 = document.getElementById("num4").value;
+  var resultado1 = parseInt(numero1) - parseInt(numero2);
+  document.getElementById("resultado1").innerHTML = resultado1;
+}
